1b1b19a8e7eb5b68594d6b73610a0ced4762fea3
Ivan Ustyantsev <vangold915@gmail.com>
2023-09-18 14:29:42 +0700

    Completed task 3

--- homework3/index.js
+++ homework3/index.js
@@ -16,19 +16,19 @@ console.log("2^3 + 3^3 =", cube(2) + cube(3));
 Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
 */
 
-function getSalary(salaryWithTax) {
+/* function getSalary(salaryWithTax) {
     clearSalary = salaryWithTax - salaryWithTax * 13 / 100;
     console.log(`Размер заработной платы за вычетом налогов равен ${clearSalary}`);
     alert(`Размер заработной платы за вычетом налогов равен ${clearSalary}`);
 }
 
-const accrual = parseInt(prompt("Введите ваши начисления: "));100
+const accrual = parseInt(prompt("Введите ваши начисления: "));
 
 if (isNaN(accrual)) {
     alert("Значение задано неверно");
 } else {
     getSalary(accrual);
-}
+} */
 
 
 /* 
@@ -36,5 +36,22 @@ if (isNaN(accrual)) {
 Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
 */
 
+/* function findMax(value1, value2, value3) {
+    if ((value1 > value2) && (value1 > value3)) {
+        return value1;
+    } else if ((value2 > value1) && (value2> value3)) {
+        return value2;
+    } 
+    return value3;
+}
+const num1 = parseInt(prompt("Введите первое число: "));
+const num2 = parseInt(prompt("Введите второе число: "));
+const num3 = parseInt(prompt("Введите третье число: "));
+
+if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+    alert("Вы ввели неправильное значение");
+} else {
+    alert(`максимальное число: ${findMax(num1, num2, num3)}`);
+} */
 
 
