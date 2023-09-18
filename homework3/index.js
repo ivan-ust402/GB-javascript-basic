fe911d52fc3c8dfb3a8273fe60ec395c638e418c
Ivan Ustyantsev <vangold915@gmail.com>
2023-09-18 14:49:40 +0700

    Completed homework 3

--- homework3/index.js
+++ homework3/index.js
@@ -2,6 +2,7 @@
 Задание 1
 Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
 */
+alert("Задание 1 (результат в консоли)");
 
 function cube(num) {
     return num*num*num;
@@ -16,7 +17,9 @@ console.log("2^3 + 3^3 =", cube(2) + cube(3));
 Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
 */
 
-/* function getSalary(salaryWithTax) {
+alert("Задание 2");
+
+function getSalary(salaryWithTax) {
     clearSalary = salaryWithTax - salaryWithTax * 13 / 100;
     console.log(`Размер заработной платы за вычетом налогов равен ${clearSalary}`);
     alert(`Размер заработной платы за вычетом налогов равен ${clearSalary}`);
@@ -28,7 +31,7 @@ if (isNaN(accrual)) {
     alert("Значение задано неверно");
 } else {
     getSalary(accrual);
-} */
+}
 
 
 /* 
@@ -36,7 +39,8 @@ if (isNaN(accrual)) {
 Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
 */
 
-/* function findMax(value1, value2, value3) {
+alert("Задание 3");
+function findMax(value1, value2, value3) {
     if ((value1 > value2) && (value1 > value3)) {
         return value1;
     } else if ((value2 > value1) && (value2> value3)) {
@@ -52,7 +56,7 @@ if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
     alert("Вы ввели неправильное значение");
 } else {
     alert(`максимальное число: ${findMax(num1, num2, num3)}`);
-} */
+}
 
 /* 
 Задание 4
@@ -63,6 +67,7 @@ if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
 4. Деление
 Необходимо сделать так, чтобы функция вернула число, например выражение console.log(sum(2, 6)); должно вывести число 8 в консоль (sum - функция сложения в данном примере, ваши названия функций могут отличаться). Округлять значения, которые возвращают функции не нужно, однако, обратите внимание на разность, функция должна вычесть из большего числа меньшее, либо вернуть 0, если числа равны. Функциям всегда передаются корректные числа, проверки на NaN, Infinity делать не нужно.
 */
+alert("Задание 4");
 
 function sum(num1, num2) {
     return num1 + num2;
@@ -90,7 +95,7 @@ function div(num1, num2) {
     return num1 / num2;
 }
 
-alert("Задание 4");
+
 
 const number1 = parseInt(prompt("Введите первое число: "));
 const number2 = parseInt(prompt("Введите второе число: "));
