38bc2daaf4878adb4d72c37dd55e63806a4890a9
Ivan Ustyantsev <vangold915@gmail.com>
2023-09-19 18:12:46 +0700

    Completed homework 4

--- homework4/index.js
+++ homework4/index.js
@@ -62,3 +62,26 @@ console.log(sum3);
 console.log(min3);
 console.log(existThree);
 console.log('');
+
+/* 
+Задание 4
+*Необязательное задание. *
+Необходимо вывести горку в консоль (используя цикл for), как показано на рисунке, только у вашей горки должно быть 20 рядов, а не 5:
+
+x
+xx
+xxx
+xxxx
+xxxxx
+*/
+
+console.log('Задание 4');
+console.log('Необязательное задание');
+
+for (let i = 0; i < 20; i++) {
+    let str = '';
+    for (let j = 0; j < i; j++) {
+        str+='*';
+    }
+    console.log(str);
+}
